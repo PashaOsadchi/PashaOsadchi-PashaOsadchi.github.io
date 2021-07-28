ea60276f1366f1dafee2dd87b24093d4d36f525b
PashaOsadchi <pashka.osadchi@gmail.com>
2021-07-28 09:13:06 +0300

    Рефакторинг

--- js/view_video.js
+++ js/view_video.js
@@ -13,17 +13,17 @@ function view_video_1() {
     document.getElementById("iframe_video_content").innerHTML = `
     <div class="iframe_video">
 
-        <iframe src="https://www.youtube.com/embed/G_ect90WitA" width="${width}" height="${height_2}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/m4cKynyRREk" width="${width}" height="${height_2}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-        <iframe src="https://www.facebook.com/plugins/video.php?height=${height_1}&href=https%3A%2F%2Fwww.facebook.com%2Fosadchyi.p%2Fvideos%2F1838327683015610%2F&show_text=false&width=${width}&t=0" width="${width}" height="${height_2}" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
-        
-        <iframe src="https://www.facebook.com/plugins/video.php?height=${height_1}&href=https%3A%2F%2Fwww.facebook.com%2Fosadchyi.p%2Fvideos%2F1838336223014756%2F&show_text=false&width=${width}&t=0" width="${width}" height="${height_2}" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+        <iframe src="https://www.youtube.com/embed/1J3fiKKOEGI" width="${width}" height="${height_2}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-        <iframe src="https://www.facebook.com/plugins/video.php?height=${height_1}&href=https%3A%2F%2Fwww.facebook.com%2Fosadchyi.p%2Fvideos%2F1838343079680737%2F&show_text=false&width=${width}&t=0" width="${width}" height="${height_2}" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+        <iframe src="https://www.youtube.com/embed/9ZWo2gsTltQ" width="${width}" height="${height_2}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-        <iframe src="https://www.facebook.com/plugins/video.php?height=${height_1}&href=https%3A%2F%2Fwww.facebook.com%2Fosadchyi.p%2Fvideos%2F1838353019679743%2F&show_text=false&width=${width}&t=0" width="${width}" height="${height_2}" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+        <iframe src="https://www.youtube.com/embed/xw0zPi-p_Vg" width="${width}" height="${height_2}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     
     </div>`;
+
+    // <iframe src="https://www.facebook.com/plugins/video.php?height=${height_1}&href=https%3A%2F%2Fwww.facebook.com%2Fosadchyi.p%2Fvideos%2F1838327683015610%2F&show_text=false&width=${width}&t=0" width="${width}" height="${height_2}" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
 
     document.getElementById("id_page_header").style.display = "none";
     document.getElementById("map").style.display = "none";
